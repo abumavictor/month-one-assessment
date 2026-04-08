304d954eb856ea5cbca1b30ece892023ed1d4286
abuma victor <abumavictor@gmail.com>
2026-04-08 22:23:54 +0100

    Add outputs for VPC and related resources
    Define outputs for VPC, subnets, NAT gateways, ALB, and bastion host.

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+output "vpc_id" {
+  value = aws_vpc.techcorp_vpc.id
+}
+
+output "public_subnet_ids" {
+  value = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+output "nat_gateway_ips" {
+  value       = aws_eip.nat_eip[*].public_ip
+  description = "The public IP addresses of the NAT Gateways"
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.techcorp_alb.dns_name
+  description = "The DNS name of the load balancer to access the website"
+}
+
+output "bastion_public_ip" {
+  value       = aws_instance.bastion.public_ip
+  description = "The public IP address of the bastion host"
