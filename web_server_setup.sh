64479a7fb323732df3924085eca668ac56e43c07
abuma victor <abumavictor@gmail.com>
2026-04-08 23:26:55 +0100

    Update web server setup script to include instance ID

--- web_server_setup.sh
+++ web_server_setup.sh
@@ -3,4 +3,6 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "<h1>Welcome to TechCorp Web Server - Deployed by Abuma Victor</h1>" > /var/www/html/index.html
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id)
+echo "<h1>TechCorp Web Server - Success</h1><p>Instance ID: $INSTANCE_ID</p>" > /var/www/html/index.html
