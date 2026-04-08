329360e2819f4c71a030a7b0870c8d782c23daa0
abuma victor <abumavictor@gmail.com>
2026-04-08 23:45:29 +0100

    Refactor variables in variables.tf for clarity and updates
    Updated variable descriptions and types, added instance type for database server, and modified default IP for Bastion SSH access.

--- variables.tf
+++ variables.tf
@@ -1,27 +1,31 @@
 variable "aws_region" {
   description = "The AWS region to deploy in"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "my_ip" {
-  description = "Your local IP address for Bastion SSH access"
+  description = "Your local IP address for Bastion SSH access (Placeholder for security)"
   type        = string
-  default     = "105.127.17.81/32"
+  default     = "0.0.0.0/0"
 }
 
 variable "instance_type_web" {
@@ -30,6 +34,17 @@ variable "instance_type_web" {
   default     = "t3.micro"
 }
 
+variable "instance_type_db" {
+  description = "The instance type for the database server"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair"
+  type        = string
+  default     = "your-key-name-here"
+}
 variable "key_name" {
   description = "The name of the SSH key pair"
   type        = string
